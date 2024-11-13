4dc21210003c56331a213c002baf1908932dbcb0
Teeg93 <samuel_teague@live.com.au>
2024-11-13 12:17:39 +1030

    Better tracking

--- src/ThermalTools.py
+++ src/ThermalTools.py
@@ -54,12 +54,20 @@ def compute_thermal_bracket(frame, min_val=27300, max_val=37300, count_threshold
 
 
 class ThermalFlow:
-    def __init__(self):
-        self.previous_frame = None
-        self.p0 = None
+    def __init__(self, redetection_interval=5, maximum_track_len=10):
+        """
 
+        :param redetection_interval: Number of frames after which points will be recalculated
+        """
+
+        self.previous_frame = None
         self.current_frame = None
-        self.p1 = None
+        self.frame_idx = 0
+
+        self.redetection_interval = redetection_interval
+        self.maximum_track_len = maximum_track_len
+
+        self.tracks = []
     
         self.feature_params = dict( maxCorners = 100,
                                     qualityLevel = 0.03,
@@ -75,9 +83,55 @@ class ThermalFlow:
         _min_temp, _max_temp = compute_thermal_bracket(frame)
         frame_8_bit = raw_to_thermal_frame(frame, _min_temp, _max_temp)
 
-        # Ignore temperatures that are clipping
-        mask = cv2.inRange(frame_8_bit, 1, 254)
+        # Create a 3-channel colour frame for display purposes
+        display_frame = np.reshape(frame_8_bit, (frame_8_bit.shape[0], frame_8_bit.shape[1], 1))
+        display_frame = cv2.cvtColor(display_frame, cv2.COLOR_GRAY2BGR)
+        print("Created display frame")
 
+        if len(self.tracks) > 0:
+            img0, img1 = self.previous_frame, frame_8_bit
+
+            p0 = np.float32([tr[-1] for tr in self.tracks]).reshape(-1, 1, 2)
+            p1, st, err = cv2.calcOpticalFlowPyrLK(img0, img1, p0, None, **self.lk_params)
+            p0r, st, err = cv2.calcOpticalFlowPyrLK(img1, img0, p1, None, **self.lk_params)
+            d = abs(p0-p0r).reshape(-1, 2).max(-1)
+            good = d < 1
+            new_tracks = []
+            for tr, (x, y), good_flag in zip(self.tracks, p1.reshape(-1, 2), good):
+                if not good_flag:
+                    continue
+                tr.append((x, y))
+                if len(tr) > self.maximum_track_len:
+                    del tr[0]
+                new_tracks.append(tr)
+                cv2.circle(display_frame, (int(x), int(y)), 2, (0, 255, 255), -1)
+            self.tracks = new_tracks
+            cv2.polylines(display_frame, [np.int32(tr) for tr in self.tracks], False, (0, 255, 0))
+
+        if self.frame_idx % self.redetection_interval == 0:
+        # Ignore temperatures that are clipping
+            mask = cv2.inRange(frame_8_bit, 1, 254)
+            print("Created Mask")
+
+            # Ignore existing tracking points
+            for x, y in [np.int32(tr[-1]) for tr in self.tracks]:
+                cv2.circle(mask, (x, y), 5, 0, -1)
+            print("Removed existing tracks from mask")
+            
+            p = cv2.goodFeaturesToTrack(frame_8_bit, mask=mask, **self.feature_params)
+            print("Detected features")
+
+            if p is not None:
+                for x, y in np.float32(p).reshape(-1, 2):
+                    self.tracks.append([(x, y)])
+                    print("Appending new tracks")
+        
+        self.previous_frame = frame_8_bit
+        return display_frame
+            
+
+
+        """
         if self.previous_frame is None:
             self.p0 = cv2.goodFeaturesToTrack(frame_8_bit, mask=mask, **self.feature_params)
             self.previous_frame = frame_8_bit
@@ -102,6 +156,7 @@ class ThermalFlow:
             img = cv2.add(frame_display, mask)
 
             self.p0 = good_new.reshape(-1, 1, 2)
+        """
         
         if img is not None:
             return img
